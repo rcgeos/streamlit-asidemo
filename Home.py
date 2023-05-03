bf0952cf1c64763d8b66a8c14fcd71284ac26b3a
rcgeos <renato.cumani4@gmail.com>
2023-05-03 14:52:40 -0400

    testing filters

--- Home.py
+++ Home.py
@@ -10,10 +10,22 @@ def main():
     st.caption(APP_SUB_TITLE)
 
     # ADD Data
-    #@st.cache_data
+    @st.cache_data
     url_path='https://raw.githubusercontent.com/rcgeos/streamlit-asidemo/main/data/data_sample.csv'
     df=pd.read_csv(url_path, encoding='ISO-8859-1')
+    
+    year = 2022 
+    month = 1
+    landcover = 'Crop Area'
+    season = 'Season 1'
+    country_name = 'Guatemala'
+    df=df[(df['Year']==year) & (df['Month']==month) & (df['Land_Type']==landcover) & (df['Season']==season)]
+    if country_name:
+        df=df[df['ISO3']==country_name]
+
+    st.write(df.shape)
     st.write(df)
+    st.write(df.columns)
 
     # Display Filters and map 
 
